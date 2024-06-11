ae5240829d219763ee16163777cda2df53bcf603
thasneemshyju86 <thasneemshyju86@gmail.com>
2024-06-11 14:07:08 -0700

    updated styles

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -9,7 +9,8 @@ const Intro = () => {
           <span className='hello'>Hello,</span>
           <span className='introText'>I'm <span className='introName'>THASNEEM SHYJU,</span><br/>Frontend Web Developer <br/>from Tracy,CA</span>
        <p className='introPara'>I am a skilled web developer with experience in creating <br/>visually appealing and user friendly websites.</p>
-          <button className='btn'>Hire Me</button>
+          <button className='btn' onClick={()=>{
+                document.getElementById('contactMe').scrollIntoView({behaviour:'smooth'})}}>Hire Me</button>
       </div>
     <img src={profile} alt='' className='profilePic'/>
        </section>
--- src/components/Skill/about.js
+++ src/components/Skill/about.js
@@ -1,27 +1,50 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
 
 const About = () => {
-return(
-        <div class='main' id='about' >
-           <h1  id="aboutMe">ABOUT ME</h1>
-           <div class="container">
-         <h1 class="item1">Get To Know Me </h1>
-         <span class="desc">Hello! <br/>I'm <span class='highlight'>Thasneem Shyju</span>, a self taught frontend developer with a passion for crafting engaging and intuitive user experiences.
-         I took my<span class='highlight'> Bachelors in Computer Science</span> and got placed in Accenture through campus recruitment and had to drop my career for my family. I am a mom of 2, who spend most of the time for the family but had a strong passion to learn and update myself and get back to work.<br/><br/>I specialize in 
-         <span class='highlight'> HTML,CSS & JavaScript</span>, with expertise in popular framework like<span class='highlight'> React</span>. I am also experienced in <span class='highlight'>responsive design, cross-browser compatibility</span>, and version control systems like <span class='highlight'>Git</span>.
-         I have had the pleasure of working on diverse projects, from creating sleek landing pages to developing interactive webpages. 
-         I'm deeply passionate about creating seamless digital experiences that delight users.<br/><br/>In the future, I aim to further
-         refine my skills, collaborate with talented teams, and contribute to projects that make a meaningful impact.
-         I'm thrilled to continue my journey in frontend development, constantly learning and pushing the boundaries of what's possible.If you are looking for a dedicated and 
-         innovative frontend developer, I'd love to connect and explore how we can work together.     </span>
-       
-         </div>
-         <div class="container1">
-          <h1 class="item2">Skills/ Tech Used</h1>
-          <button>HTML</button>
-          <button>CSS</button>
-          <button>JavaScript</button>
+  return (
+    <div class="main" id="about">
+      <h1 id="aboutMe">ABOUT ME</h1>
+      <div class="container">
+        <h1 class="item1">Get To Know Me </h1>
+        <span class="desc">
+          Hello! <br />
+          I'm <span class="highlight">Thasneem Shyju</span>, a self taught
+          frontend developer with a passion for crafting engaging and intuitive
+          user experiences. I took my
+          <span class="highlight"> Bachelors in Computer Science</span> and got
+          placed in Accenture through campus recruitment and had to drop my
+          career for my family. I am a mom of 2, who spend most of the time for
+          the family but had a strong passion to learn and update myself and get
+          back to work.
+          <br />
+          <br />I specialize in
+          <span class="highlight"> HTML,CSS & JavaScript</span>, with expertise
+          in popular framework like<span class="highlight"> React</span>. I am
+          also experienced in{" "}
+          <span class="highlight">
+            responsive design, cross-browser compatibility
+          </span>
+          , and version control systems like <span class="highlight">Git</span>.
+          I have had the pleasure of working on diverse projects, from creating
+          sleek landing pages to developing interactive webpages. I'm deeply
+          passionate about creating seamless digital experiences that delight
+          users.
+          <br />
+          <br />
+          In the future, I aim to further refine my skills, collaborate with
+          talented teams, and contribute to projects that make a meaningful
+          impact. I'm thrilled to continue my journey in frontend development,
+          constantly learning and pushing the boundaries of what's possible.If
+          you are looking for a dedicated and innovative frontend developer, I'd
+          love to connect and explore how we can work together.{" "}
+        </span>
+      </div>
+      <div class="container1">
+        <h1 class="item2">Skills/ Tech Used</h1>
+        <button>HTML</button>
+        <button>CSS</button>
+        <button>JavaScript</button>
         <button>React</button>
         <button>BootStrap</button>
         <button>Tailwind CSS</button>
@@ -31,11 +54,8 @@ return(
         <button>Jenkins</button>
         <button>Agile</button>
         <button>Responsive design</button>
-         
-        </div>
-       
-        </div>
-
-    );
-}
-export default About
+      </div>
+    </div>
+  );
+};
+export default About;
